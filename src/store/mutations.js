f32db4ae0ff8e2afdaf2c470eccbff1c561c09e0
LuoRiWuSheng <1197802519@qq.com>
2018-10-14 12:10:32 +0800

    修改src/store/mutations.js中的错误
    这里新增图书，如果购物车存在，则只在数量上加1，且原来的product是一个布尔类型，原来的写法是 product.bookCount +=1  应该是笔误

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,9 +2,8 @@ import * as Types from './mutations-type';
 const mutations={
   [Types.ADD_CART](state,book){
     let product=state.cartList.find(item=>item.bookId==book.bookId);
-    if(product){
-      product.bookCount+=1;
-      state.cartList=[...state.cartList];
+    if(product){ // 购物车有添加的这本书，则只在数量上加1
+      book.bookCount+=1;
     }else{
       book.bookCount=1;
       state.cartList=[...state.cartList,book]
